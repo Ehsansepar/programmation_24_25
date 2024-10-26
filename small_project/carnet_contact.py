e89df1a928f399225ec8580ca99bc317c5976371
Ehsansepar <Ehsan.separ007@gmail.com>
2024-10-26 23:58:26 +0200

    supprimer contact, rechercher, et les autres complete

--- small_project/carnet_contact.py
+++ small_project/carnet_contact.py
@@ -16,11 +16,10 @@ def ajouter_contact() :
     nom = input("Entrez le nom du contact : ")
     numero = input("Entrez le numéro de téléphone du contact : ")
     email = input("Entrez l'email du contact : ")
+    print("C'est bon  ! Le contact a été ajouté avec succès !")
+    print("-----\n\n")
+
     all_contacts.append([nom, numero, email])
-
-
-
-ajouter_contact()
 
 def afficher_contacts():
     if not all_contacts:
@@ -33,12 +32,45 @@ def afficher_contacts():
             print("-----")
 
 def  rechercher_contact():
+    i = 1
     nom = input("Entrez le nom du contact à rechercher : ")
+    trouve = False
     for  contact in all_contacts:
-        if contact[0] == nom:
+        if contact[0].lower() == nom.lower() :
+            print("Id",  i)
             print("Nom:", contact[0])
             print("Numéro de téléphone:", contact[1])
             print("Email:", contact[2])
             print("-----")
-        else :  print("Contact non trouvé, reesayer")
+            trouve = True
+            i += 1
+            break
+        if not trouve :
+            print("Aucun contact trouvé avec ce nom.")
 
+def supprimer_contact():
+    i = 1
+    nom = input("Entrez le nom du contact à supprimer : ")
+    trouve = False
+    for contact in all_contacts:
+        if contact[0].lower() == nom.lower():
+            del all_contacts[i-1]
+            print("Le contact a été supprimé avec succès !")
+            trouve = True
+            break
+        i += 1
+
+
+while True : 
+    choisir  = input("Voulez-vous ajouter un contact (A), afficher tous les contacts (B), rechercher un contact (C) ou supprimer un contact (D) ? ")
+
+    if choisir.upper() == "A" :
+        ajouter_contact()
+    elif choisir.upper() == "B" :
+        afficher_contacts()
+    elif choisir.upper() == "C" :
+        rechercher_contact()
+    elif  choisir.upper() == "D" :
+        supprimer_contact()
+    else :
+        print("Option non valide.")
