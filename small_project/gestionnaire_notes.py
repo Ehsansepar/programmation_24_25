2f9f86d7cfab451d47a93f7274efa0c22deabefe
Ehsansepar <Ehsan.separ007@gmail.com>
2024-10-29 23:10:02 +0100

    gestionnaire note complete

--- small_project/gestionnaire_notes.py
+++ small_project/gestionnaire_notes.py
@@ -7,7 +7,18 @@ students = [
 # for i , student in enumerate(students):
 #     print(f"Nom: {student[0]}, Math: {student[1]}, Physique{student[2]}, Chimie: {student[3]}")
 
-for i in range(len(students)) : 
-    result =  sum(students[i][1:4])
-    print(f"Nom: {students[i][0]}, Note moyenne: {result/3}")
 
+meilleure_moyenne = 0
+meilleure_etudiant = ""
+
+for i in range(len(students)) : 
+    somme_notes =  sum(students[i][1:4])
+    moyenne =  somme_notes / 3
+    print(f"Nom: {students[i][0]}, Note moyenne: {round(moyenne, 2)}")
+    
+    if moyenne > meilleure_moyenne :
+        meilleure_moyenne = moyenne
+        meilleure_etudiant = students[i][0]
+        # print(f"Meilleur élève: {meilleure_etudiant}")
+print()
+print("Meilleur  élève: ", meilleure_etudiant, "\nla moyenne de ", round(meilleure_moyenne, 2))
