78ec97ae561001689a06ee31773705db2a793f1a
Ehsansepar <Ehsan.separ007@gmail.com>
2024-10-31 23:29:21 +0100

    ajouter la menu pour gestionnaire_tache.py

--- small_project/gestionnaire_tache.py
+++ small_project/gestionnaire_tache.py
@@ -1,6 +1,6 @@
 taches = []
 
-def ajoute_tache() : 
+def ajouter_tache() : 
     print(" --- option : Ajouter la tâche --- ")
     nom = input("Entrer le nom de tâche : ")
     description = input("Entrez la discription de la tâche : ")
@@ -23,5 +23,30 @@ def afficher_taches() :
 
 
 
-ajoute_tache()
-afficher_taches()
+def menu_principal():
+    while True:
+        print("\n--- Gestionnaire de Tâches ---")
+        print("1 - Ajouter une nouvelle tâche")
+        print("2 - Afficher toutes les tâches")
+        print("3 - Marquer une tâche comme terminée")
+        print("4 - Supprimer une tâche")
+        print("5 - Quitter")
+
+        choix = input("Choisissez une option (1-5) : ")
+
+        if choix == "1":
+            ajouter_tache()
+        elif choix == "2":
+            afficher_taches()
+        elif choix == "3":
+            marquer_tache_terminee()
+        elif choix == "4":
+            supprimer_tache() 
+        elif choix == "5":
+            print("Au revoir !")
+            break
+        else:
+            print("Choix invalide. Veuillez entrer un nombre entre 1 et 5.")
+
+# Lancer le programme
+menu_principal()
