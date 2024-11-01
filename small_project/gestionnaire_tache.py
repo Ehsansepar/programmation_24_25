237b386faa4c240c24f42f64aa2cec8c484dfb59
Ehsansepar <Ehsan.separ007@gmail.com>
2024-11-01 23:24:24 +0100

    gestionnaire_tache.py added the functionality to delete and status updated

--- small_project/gestionnaire_tache.py
+++ small_project/gestionnaire_tache.py
@@ -6,6 +6,7 @@ def ajouter_tache() :
     description = input("Entrez la discription de la tâche : ")
     statut = "en attente"
     taches.append([nom, description, statut])
+    print("\n ---*--- Votre tâche a enregistrer avec succés ---*---")
 
 def afficher_taches() : 
     if not taches : 
@@ -16,10 +17,55 @@ def afficher_taches() :
         #     nom = tache[0]
         #     description = tache[1]
         #     statut = tache[2]
+        print("     --- Affichage des tâches ---    ")
         for i, tache in enumerate(taches, 1):
             nom, description, statut = tache
-        print("     --- Affichage des tâches ---    ")
-        print(f"{i} - Nom : {nom}\n    Description : {description}\n    Statut : {statut}")
+            print(f"{i} - Nom : {nom}\n    Description : {description}\n    Statut : {statut}\n -----------------------")
+
+
+def supprimer_tache() : 
+    if not taches :
+        print(" --- Aucune tâche enregistrée --- ")
+        return
+    
+    afficher_taches()
+
+    while True : 
+        index = int(input("Entrez le numéro de la tâche à supprimer : "))
+
+        try : 
+            if index < 0 :
+                print("Le numéro est invalide. Entrez un nombre positif : ")
+            elif index > len(taches) :
+                print("Numéro de tâche est invalid. Entrez un nombre dans la liste : ")
+            else :
+                taches.pop(index-1)
+                print("Votre tâche a supprimé avec succés")
+                break
+        except ValueError : 
+            print("Entrez un nombre valide.")
+
+def marquer_tache_terminee() : 
+    if not taches : 
+        print(" --- Aucune tâche enregistrée --- ")
+        return
+    
+    afficher_taches()
+
+    while True : 
+        try : 
+            nombre_tache = int(input("Entrez le numéro de la tâche à marqué comme terminée : "))
+
+            if nombre_tache < 0 :
+                print("Entrez un nombre positif. ")
+            elif nombre_tache > len(taches) : 
+                print("le nombre est invalide. Entrez un nombre dans la liste. ")
+            else : 
+                taches[nombre_tache - 1][2] = "terminée"
+                print(f"Votre tâche '{taches[nombre_tache - 1][0]}' marquée comme terminée. ")
+                break
+        except ValueError :
+            print("Entrez un nombre valide. ")
 
 
 
