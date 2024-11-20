7f599a3ee5a9a6ac21a678bd06fcffc3cc56b6e7
Ehsansepar <ehsan.separ007@gmail.com>
2024-11-20 22:59:20 +0100

    normal commit

--- prepration for test list/test.py
+++ prepration for test list/test.py
@@ -163,10 +163,10 @@
 # Affichez le minimum (cf. Maximum)
 # On initialise mini=nb[0] et pas mini=0
 # Sauvez sous liste6.py
-nb=[25,89,65,99,13,78,12,33,68,17]
-mini = nb[0]
+# nb=[25,89,65,99,13,78,12,33,68,17]
+# mini = nb[0]
 
-for i in range(1, len(nb)) : 
-    if nb[i] < mini : 
-        mni = nb[i]
-print(mini)
+# for i in range(1, len(nb)) : 
+#     if nb[i] < mini : 
+#         mni = nb[i]
+# print(mini)
